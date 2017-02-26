4503d0ef11078df9afd5df5c5c8f8c6e9afd2dab
Michelle Zeng <zlqfly@gmail.com>
2017-02-27 07:37:45 +1100

    single it function

--- js/hello-test.js
+++ js/hello-test.js
@@ -4,17 +4,19 @@ function it(desc, fn) {
 	try{
 		fn();
 	} catch (e) {
-		console.log('assertion fails!');
+		console.log('assertion fails!', e.name + ':' + e.message);
+		console.log(e.stack);
 	}
 }
 
 console.log('start running test');
 
+
 it('3 should equal 4', function () {
 	expect(3).to.equal(4);	
 })
 
-
+// run multiple it functions
 
 
 
